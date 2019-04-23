e3772d2227c9bdafe94820281e4287a61f5b1ee9
vuminhtam <apple@vuminhtam.local>
2019-04-23 10:15:26 +0700

    a

---
+++ QuanTro/Controller/tabar/CELL2_Table_Tabar_Custom_Search_01_TableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CELL2_Table_Tabar_Custom_Search_01_TableViewCell.swift
+//  QuanTro
+//
+//  Created by vuminhtam on 4/22/19.
+//  Copyright © 2019 Le Nguyen Quoc Cuong. All rights reserved.
+//
+
+import UIKit
+
+class CELL2_Table_Tabar_Custom_Search_01_TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lb_1: UILabel!
+    static let CELL2 = "CELL2"
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
